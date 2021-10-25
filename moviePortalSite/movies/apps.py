077fe83bc3fb2156161c4d6e01b572aae4a71b3c
Bhuwan <dev@bhuwansharma.com>
2021-10-26 00:10:00 +0530

    :truck: moviePortalApp -> movies
    renaming

--- moviePortalSite/movies/apps.py
+++ moviePortalSite/movies/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MovieportalappConfig(AppConfig):
+class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'moviePortalApp'
+    name = 'movies'
