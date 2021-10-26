c267e01e45e20d524685f375e9d9804183bcc48e
Bhuwan <dev@bhuwansharma.com>
2021-10-26 15:13:41 +0530

    :sparkles: movie model
    migrations have not been generated yet

--- moviePortalSite/movies/models.py
+++ moviePortalSite/movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    title_original = models.CharField(max_length=255)
+    format = models.TextChoices(
+        '65mm',
+        '75mm',
+        '35mm'
+    )
+    slug = models.CharField(max_length=255, primary_key=True)
+    running_time = models.IntegerField()
+    trailer = models.TextField()
+    plot = models.TextField()
+    released_on = models.DateField()
+    created_at = models.DateTimeField()
