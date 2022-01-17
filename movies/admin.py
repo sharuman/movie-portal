48e6607477888cf21f1aca5091acf48745008544
RobaM3 <r.mobtasem@nu.edu.eg>
2022-01-17 15:13:58 +0100

    Updated Models - Created Viewer & Genre  models

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Persona
+from .models import Genre, Persona, Viewer
 from .models import Movie
 
 admin.site.register(Movie)
 admin.site.register(Persona)
+admin.site.register(Viewer)
+admin.site.register(Genre)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,4 @@
+from random import choices
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -7,23 +8,41 @@ class Persona(models.Model):
     
     # Persona is representative of both, actors/actresses and directors
     
-    full_name = models.CharField(max_length=200)
+    TYPE_CHOICES = (
+        (0, 'Both'),
+        (1, 'Actor'),
+        (2, 'Director'),
+    )
+
+    full_name = models.CharField(max_length=200, primary_key=True)
+    type = models.IntegerField(choices=TYPE_CHOICES)
+
+class Genre(models.Model):
     
+    # Genre is representative of the type of the movie
+    
+    name = models.CharField(max_length=200, primary_key=True)    
+
+class Viewer(models.Model):
+    
+    # User is representative of information of users who interact with the website
+    
+    full_name = models.CharField(max_length=300, primary_key=True)
 
 class Movie(models.Model):
 
     # Represents all the movie details
 
-    movie_title = models.CharField(max_length=300)
-    movie_length = models.FloatField()
-    movie_genre = models.CharField(max_length=300)
-    star_rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], blank=True)
+    title = models.CharField(max_length=300, primary_key=True)
+    length = models.FloatField()
+    star_rating = models.ManyToManyField(Viewer, related_name='assigned_by', blank=True)
     released_on = models.DateField()
     added_on = models.DateTimeField(auto_now_add=True)
     director = models.ManyToManyField(Persona, related_name='movie_director')
     actor = models.ManyToManyField(Persona, related_name='movie_actor')
-    movie_trailer = models.URLField(max_length=500, blank=True, default=None)
-    movie_plot = models.TextField()
+    genre = models.ManyToManyField(Genre, related_name='movie_genre')
+    trailer = models.URLField(max_length=500, blank=True, default=None)
+    plot = models.TextField()
 
     
     # class Meta:
