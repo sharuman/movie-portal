a20bdff44c20b1bf08ec667a46a62527f73254bf
RobaM3 <r.mobtasem@nu.edu.eg>
2022-01-18 03:53:11 +0100

    Updated models - Created class Rating for M2M relationship between User & Movie

--- movies/admin.py
+++ movies/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Genre, Persona, Viewer
+from .models import Genre, Persona, Rating
 from .models import Movie
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"m_slug": ("title",)}
+
+class GenreAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"g_slug": ("name",)}
+
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Persona)
-admin.site.register(Viewer)
-admin.site.register(Genre)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Rating)
--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,8 @@
 from random import choices
+from turtle import update
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,35 +11,40 @@ class Persona(models.Model):
     # Persona is representative of both, actors/actresses and directors
     
     TYPE_CHOICES = (
-        (0, 'Both'),
+        (0, 'Director'),
         (1, 'Actor'),
-        (2, 'Director'),
     )
 
-    full_name = models.CharField(max_length=200, primary_key=True)
+    full_name = models.CharField(max_length=200)
     type = models.IntegerField(choices=TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Genre(models.Model):
     
     # Genre is representative of the type of the movie
     
-    name = models.CharField(max_length=200, primary_key=True)    
+    name = models.CharField(max_length=200, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    g_slug = models.SlugField(max_length=254, unique=True)
 
-class Viewer(models.Model):
-    
-    # User is representative of information of users who interact with the website
-    
-    full_name = models.CharField(max_length=300, primary_key=True)
+    def __str__(self):
+       return self.name    
+
 
 class Movie(models.Model):
 
     # Represents all the movie details
 
-    title = models.CharField(max_length=300, primary_key=True)
+    title = models.CharField(max_length=300)
+    m_slug = models.SlugField(max_length=254, unique=True)
     length = models.FloatField()
-    star_rating = models.ManyToManyField(Viewer, related_name='assigned_by', blank=True)
+    viewer_rating = models.ManyToManyField(User, through='Rating', blank=True)
     released_on = models.DateField()
-    added_on = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     director = models.ManyToManyField(Persona, related_name='movie_director')
     actor = models.ManyToManyField(Persona, related_name='movie_actor')
     genre = models.ManyToManyField(Genre, related_name='movie_genre')
@@ -45,12 +52,16 @@ class Movie(models.Model):
     plot = models.TextField()
 
     
-    # class Meta:
-    #     ordering = ["title"]
-    # format = models.CharField(
-    #     max_length=5,
-    #     choices=MovieFormat.choices,
-    #     default=MovieFormat.COLOR
-    # )
+    class Meta:
+        ordering = ["title"]
+    
+    def __str__(self):
+       return self.title
    
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    star_rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)],)
 
