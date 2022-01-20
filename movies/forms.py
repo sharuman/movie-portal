904d519569ed98746f9258ba1605798bff7d0a79
RobaM3 <r.mobtasem@nu.edu.eg>
2022-01-20 02:57:21 +0100

    Updates to sign up page & sign up page authentication

---
+++ movies/forms.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+
+    # only stated fields would be used
+
+    full_name = forms.CharField(max_length=200, required=True,
+                                widget=forms.TextInput(attrs={'placeholder': 'Full Name', 'class': 'form-control',
+                                                              }))
+
+    username = forms.CharField(max_length=50, required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control',
+                                                             }))
+
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'placeholder': 'E-mail Address', 'class': 'form-control',
+                                                           }))
+
+    password = forms.CharField(max_length=50, required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control',
+                                                                 'data-toggle': 'password',
+                                                                 }))
+
+    password_confirmation = forms.CharField(max_length=50,
+                                            required=True,
+                                            widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Entered Password',
+                                                                              'class': 'form-control',
+                                                                              'data-toggle': 'password',
+                                                                              }))
+
+    class Meta:
+        # built-in User model
+        model = User
+        fields = ['full_name', 'username', 'email',
+                  'password', 'password_confirmation']
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+from contextlib import redirect_stdout
+from multiprocessing import AuthenticationError
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 import datetime
+from django.views import View
+
+# importing needed, created forms from forms.py
+from .forms import SignUpForm
 
 # Create your views here.
+
 
 def index(request):
     now = datetime.datetime.now()
     return render(request, 'index.html', {'now': now})
+
+# def home(request):
+#     return render(request, 'home.html')
+
+
+class SignUpView(View):
+
+    form_class = SignUpForm
+    initial = {'key': 'value'}
+    template_name = 'signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()        
+            return redirect(to='/')
     
+        else:
+            return render(request, self.template_name, {'form': form})
