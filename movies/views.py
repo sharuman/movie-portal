1f648002d3d37335b395a1fcc19219307a6504c2
Florian Zenzmaier <F.Zenzmaier@gmail.com>
2022-01-25 12:49:51 +0100

    Added some first functionality to homepage
    First version, to see if we can properly get the movies from the models inside the template

--- movies/views.py
+++ movies/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 import datetime
 
 # Create your views here.
+from movies.models import Movie
+
 
 def index(request):
     now = datetime.datetime.now()
-    return render(request, 'index.html', {'now': now})
-    
+    feature_movies=get_features_movies()
+    return render(request, 'index.html', {'now': now,'featured_movie_list':feature_movies})
+
+
+
+
+
+
+
+
+
+
+
+def get_features_movies()->list[Movie]:
+    return Movie.objects.all()[:5]
