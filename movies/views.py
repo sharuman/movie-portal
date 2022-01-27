323d96c2073383ec066e184df49b4143fc4309cd
Florian Zenzmaier <F.Zenzmaier@gmail.com>
2022-01-27 01:10:59 +0100

    Reformated some code, added another css again

--- movies/views.py
+++ movies/views.py
@@ -8,40 +8,35 @@ import datetime
 from movies.models import Movie
 from django.contrib.staticfiles import finders
 
+
 def index(request):
     now = datetime.datetime.now()
-    feature_movies=get_features_movies()
-    recommended_movies=get_recommended_movies()
+    feature_movies = get_features_movies()
+    recommended_movies = get_recommended_movies()
     return render(request, 'index.html', {'now': now,
-                                          'featured_movie_list':feature_movies,
-                                          'recommended_movie_list':recommended_movies
+                                          'featured_movie_list': feature_movies,
+                                          'recommended_movie_list': recommended_movies
                                           })
 
 
+def get_50_movies_with_pictures():  # temporary helper function
+    all_movies = Movie.objects.all()
+    samples = random.sample(list(all_movies), 200)
+    valid_samples = list()
 
-def get_50_movies_with_pictures():#temporary helper function
-    all_movies=Movie.objects.all()
-    samples=random.sample(list(all_movies),200)
-    valid_samples=list()
-
-    basePath=finders.find("images/posters")
+    basePath = finders.find("images/posters")
 
     for sample in samples:
-        fullPath=os.path.join(basePath,str(sample.id) + ".jpg")
+        fullPath = os.path.join(basePath, str(sample.id) + ".jpg")
         if (os.path.exists(fullPath)):
             valid_samples.append(sample)
-            if(len(valid_samples)>=50):
+            if (len(valid_samples) >= 50):
                 return valid_samples
 
 
-def get_features_movies()->list[Movie]:
-    return get_50_movies_with_pictures()
-
-def get_recommended_movies()->list[Movie]:
+def get_features_movies() -> list[Movie]:
     return get_50_movies_with_pictures()
 
 
-
-
-
-
+def get_recommended_movies() -> list[Movie]:
+    return get_50_movies_with_pictures()
